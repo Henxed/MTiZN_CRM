18e12ffe8ddfd23341bc4c6bd6183bd022f6d65a
Henxed <henxed@ya.ru>
2022-04-26 17:36:24 +0500

    rollback

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -23,7 +23,7 @@ window.Pusher = require('pusher-js');
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
-    wsHost: '193.164.16.217',
+    wsHost: window.location.hostname,
     wsPort: process.env.MIX_WEBSOCKET_PORT,
     wssPort: process.env.MIX_WEBSOCKET_PORT,
     forceTLS: false,
